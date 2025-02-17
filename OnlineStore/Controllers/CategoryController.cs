5b5e8fb4860f06712737300a55b259747d2ab8e4
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-02-17 23:20:31 +0200

    add category controller and view

---
+++ OnlineStore/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
