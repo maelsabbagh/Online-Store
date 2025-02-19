e41d6f2a4759af32e71e41384057b2c8a7b14af8
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-02-19 21:18:21 +0200

    display categories

--- OnlineStore/Controllers/CategoryController.cs
+++ OnlineStore/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Data;
 
 namespace OnlineStore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db=db;   
+        }
         public IActionResult Index()
         {
-            return View();
+            var categoriesList = _db.Categories.ToList();
+            return View(categoriesList);
         }
     }
 }
