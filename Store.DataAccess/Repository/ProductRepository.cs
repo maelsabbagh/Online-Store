ba215c9b0df144d4f5bf187786a23f9df4e94c61
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-03-02 22:58:05 +0200

    add product repository

---
+++ Store.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Store.DataAccess.Data;
+using Store.DataAccess.Repository.IRepository;
+using Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.DataAccess.Repository
+{
+    class ProductRepository : Repository<Product>,IProductRepository 
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _db = dbContext;
+        }
+
+        public void Update(Product product)
+        {
+            _db.products.Update(product);
+        }
+    }
+}
--- Store.DataAccess/Repository/UnitOfWork.cs
+++ Store.DataAccess/Repository/UnitOfWork.cs
@@ -13,11 +13,14 @@ namespace Store.DataAccess.Repository
 
         public ICategoryRepository Category { get; private set; }
 
+        public IProductRepository Product { get; set; }
+
         private readonly ApplicationDbContext _db;
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
+            Product = new ProductRepository(_db);
         }
         public void Save()
         {
