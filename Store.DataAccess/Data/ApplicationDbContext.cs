986ec09e6571067924d0905287340c9abc2bf6b1
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-03-04 23:03:06 +0200

    add foreign key to product table

--- Store.DataAccess/Data/ApplicationDbContext.cs
+++ Store.DataAccess/Data/ApplicationDbContext.cs
@@ -33,7 +33,8 @@ namespace Store.DataAccess.Data
                     ListPrice = 99,
                     Price = 90,
                     Price50 = 85,
-                    Price100 = 80
+                    Price100 = 80,
+                    CategoryId=1
                 },
                 new Product
                 {
@@ -45,7 +46,9 @@ namespace Store.DataAccess.Data
                     ListPrice = 40,
                     Price = 30,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 1
+
                 },
                 new Product
                 {
@@ -57,7 +60,9 @@ namespace Store.DataAccess.Data
                     ListPrice = 55,
                     Price = 50,
                     Price50 = 40,
-                    Price100 = 35
+                    Price100 = 35,
+                    CategoryId = 1
+
                 },
                 new Product
                 {
@@ -69,7 +74,9 @@ namespace Store.DataAccess.Data
                     ListPrice = 70,
                     Price = 65,
                     Price50 = 60,
-                    Price100 = 55
+                    Price100 = 55,
+                    CategoryId = 1
+
                 },
                 new Product
                 {
@@ -81,7 +88,9 @@ namespace Store.DataAccess.Data
                     ListPrice = 30,
                     Price = 27,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 2
+
                 },
                 new Product
                 {
@@ -93,7 +102,9 @@ namespace Store.DataAccess.Data
                     ListPrice = 25,
                     Price = 23,
                     Price50 = 22,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 1
+
                 }
                 );
 
--- Store.DataAccess/Migrations/20250304210036_AddForeignKeyToCategoryProductRelation.Designer.cs
+++ Store.DataAccess/Migrations/20250304210036_AddForeignKeyToCategoryProductRelation.Designer.cs
@@ -2,17 +2,20 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Store.DataAccess.Data;
 
 #nullable disable
 
-namespace Store.DataAccess.Data
+namespace Store.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250304210036_AddForeignKeyToCategoryProductRelation")]
+    partial class AddForeignKeyToCategoryProductRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
