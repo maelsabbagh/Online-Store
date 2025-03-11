8d5e3d7d6a64b101c98374834b7f1735efcaa019
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-03-12 01:30:36 +0200

    display products in Homepage and details button to each product

--- OnlineStore/Areas/Customer/Controllers/HomeController.cs
+++ OnlineStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Store.DataAccess.Repository.IRepository;
 using Store.Models;
 
 namespace OnlineStore.Areas.Customer.Controllers;
@@ -9,15 +10,25 @@ namespace OnlineStore.Areas.Customer.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var products = _unitOfWork.Product.GetAll(includeProperties:"Category");
+        return View(products);
+    }
+
+    public IActionResult Details(int? id)
+    {
+        if (id == 0 || id == null) return NotFound();
+        var product = _unitOfWork.Product.Get(p => p.Id == id,includeProperties:"Category");
+        return View(product);
     }
 
     public IActionResult Privacy()
