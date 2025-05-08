8975c1d6553a42bc2cec8c32b6695cffe89716f5
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-05-08 16:18:22 +0300

    admin and employees only can view all orders

--- OnlineStore/Areas/Admin/Controllers/OrderController.cs
+++ OnlineStore/Areas/Admin/Controllers/OrderController.cs
@@ -6,6 +6,7 @@ using Store.Models.ViewModels;
 using Store.Utility;
 using Stripe.Climate;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace OnlineStore.Areas.Admin.Controllers
 {
@@ -29,12 +30,12 @@ namespace OnlineStore.Areas.Admin.Controllers
             orderVM = new OrderVM()
             {
                 OrderHeader = _unitOfWork.OrderHeader.Get(o => o.Id == orderId, includeProperties: nameof(ApplicationUser)),
-                OrderDetails = _unitOfWork.OrderDetail.GetAll(o => o.OrderHeader.Id == orderId,includeProperties:nameof(Store.Models.Product))
+                OrderDetails = _unitOfWork.OrderDetail.GetAll(o => o.OrderHeader.Id == orderId, includeProperties: nameof(Store.Models.Product))
             };
             return View(orderVM);
         }
         [HttpPost]
-        [Authorize(Roles =SD.Role_Admin + ","+SD.Role_Employee)]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
         public IActionResult UpdateOrderDetails()
         {
             var orderHeaderFromDb = _unitOfWork.OrderHeader.Get(u => u.Id == orderVM.OrderHeader.Id);
@@ -62,26 +63,38 @@ namespace OnlineStore.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll(string status)
         {
-            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: nameof(ApplicationUser)).ToList();
+            IEnumerable<OrderHeader> objOrderHeaders;
 
-            switch (status)
+            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
             {
-                case "pending":
-                    objOrderHeaders = objOrderHeaders.Where(u=>u.PaymentStatus==SD.PaymentStatusDelayedPayment);
-                    break;
-                case "inprocess":
-                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusInProcess);
-                    break;
-                case "completed":
-                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusShipped);
-                    break;
-                case "approved":
-                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusApproved);
-                    break;
-                default:
-                    break;
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: nameof(ApplicationUser)).ToList();
 
             }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(o => o.ApplicationUserId == userId, includeProperties: nameof(ApplicationUser));
+            }
+                switch (status)
+                {
+                    case "pending":
+                        objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusPending);
+                        break;
+                    case "inprocess":
+                        objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusInProcess);
+                        break;
+                    case "completed":
+                        objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusShipped);
+                        break;
+                    case "approved":
+                        objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusApproved);
+                        break;
+                    default:
+                        break;
+
+                }
             return Json(new { data = objOrderHeaders });
         }
         #endregion
